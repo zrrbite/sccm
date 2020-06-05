2d0dfed5817bbaff4dbe863008864c61e2cbd3e4
isdrion <65683088+isdrion@users.noreply.github.com>
2020-06-05 18:02:54 -0500

    Initial commit

---
+++ collection_os/collection_os_ntserver.sql
@@ -0,0 +1,10 @@
+SELECT
+  SMS_R_SYSTEM.ResourceID,
+  SMS_R_SYSTEM.ResourceType,
+  SMS_R_SYSTEM.Name,
+  SMS_R_SYSTEM.SMSUniqueIdentifier,
+  SMS_R_SYSTEM.ResourceDomainORWorkgroup,
+  SMS_R_SYSTEM.Client
+FROM SMS_R_System
+WHERE SMS_R_System.OperatingSystemNameandVersion LIKE "Microsoft Windows NT Server%"
+OR SMS_R_System.OperatingSystemNameandVersion LIKE "Microsoft Windows NT Advanced Server%"
